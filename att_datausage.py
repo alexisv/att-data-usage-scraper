7970ed3eb869a18dc57e2b75325cbc15ace492ce
Alexis Villalon <alexisv@gmail.com>
2014-02-09 11:35:10 -0500

    modified:   att_datausage.py

--- att_datausage.py
+++ att_datausage.py
@@ -3,30 +3,35 @@
 # att_datausage.py
 # created by Alexis Villalon alexisv@gmail.com
 #
-import getopt
 import requests
-import re
-import string
-import sys
+import datetime
 import keyring
 import hparse
+import getopt
+import string
+import sys
 ATTURL = 'http://www.att.com'
 KEYRING_SERVICE = 'att'
+FORM1 = 'ssoLoginForm'
+FORM2 = 'tGuardLoginForm'
 
 def printhelp(this):
     print '{} -h'.format(this)
     print '{} [-d] -u <userid>'.format(this)
     print '{} [-d] --userid=<userid>'.format(this)
 
-def printres(req, debug):
+def procres(req, debug):
     if debug == 'Y':
-        print 'Status Code: {}'.format(req.status_code)
-        print 'URL now: {}'.format(req.url)
-        print 'History: {}'.format(req.history)
+        dprint('Status Code: {}'.format(req.status_code), debug)
+        dprint('URL now: {}'.format(req.url), debug)
+        dprint('History: {}'.format(req.history), debug)
+    if req.status_code != 200:
+        print 'ERROR: Web server response status is ', req.status_code
+        return(1)
 
 def dprint(message, debug):
     if debug == 'Y':
-        print message
+        print 'debug:', str(datetime.datetime.now()), ':', message
 
 def main():
     this = sys.argv[0]
@@ -48,8 +53,18 @@ def main():
     if userid == '':
         printhelp(this)
         return(3)
-    info = getdatausage(userid, debug)
-    if not (info is None):
+    d1, d2 = getdatausage(userid, debug)
+    if d1 in (199, 209, 219, 229, 239, 249, 259, 299, 349, 399):
+        return(d1)
+    elif not (d1 is None):
+        print '----------------------------------------------------------------'
+        print "Billing Period: {0}; {1}".format(d1['timerange'], d1['daysleft'])
+        print 'Detailed Data Usage Status: {}'.format(d1['totalusage'])
+        print 'Detailed Data Usage per device:'
+        for dphone_number in d2:
+            dphone_owner = d2[dphone_number]['name']
+            dphone_data = d2[dphone_number]['data']
+            print '\t{0} : {1} : {2}'.format(dphone_owner, dphone_number, dphone_data)
         return
     else:
         return(4)
@@ -66,81 +81,96 @@ def getdatausage(userid, debug):
         print "Debug is set."
     dprint('Connecting to {}'.format(ATTURL), debug)
     r = requests.get(ATTURL)
-    printres(r, debug)
-    dprint('Looking for form with id ssoLoginForm', debug)
+    resisnot200 = procres(r, debug)
+    if resisnot200:
+        return(199)
+    dprint('Looking for form with id {}'.format(FORM1), debug)
     hparser = hparse.getforms()
     hparser.feed(r.text)
-    frm1attr, frm1inps = hparser.get_forms_data() 
+    frm1attr, frm1inps = hparser.get_forms_data()
     dprint('Getting method, action, and input fields from the form...', debug)
-    form1_method = frm1attr['ssoLoginForm']['method']
-    form1_action = frm1attr['ssoLoginForm']['action']
+    form1_method = frm1attr[FORM1]['method']
+    form1_action = frm1attr[FORM1]['action']
     form1_payload = {}
-    for k in (frm1inps['ssoLoginForm'].viewkeys()):
+    for k in (frm1inps[FORM1].viewkeys()):
         if k == 'wireless_num':
             form1_payload[k] = userid
         elif k == 'pass':
             form1_payload[k] = password
         else:
-            form1_payload[k] = frm1inps['ssoLoginForm'][k]
-    if string.lower(form1_method) != 'post':
-        print("ERROR: FORM method is not post! exiting.")
-        return
-    dprint('Data payload: {}'.format(form1_payload), debug)
+            form1_payload[k] = frm1inps[FORM1][k]
+    if string.lower(form1_method) != 'post' or form1_action in (None, '', ' '):
+        print('ERROR: FORM details are not as expected! ', frm1attr[FORM1], frm1inps[FORM1])
+        return(249)
+    #dprint('Data payload: {}'.format(form1_payload), debug)
     dprint('Connecting to {}'.format(form1_action), debug)
     r2 = requests.post(form1_action, data=form1_payload)
-    printres(r2, debug)
-    dprint('Looking for form with id tGuardLoginForm', debug)
-    #hparser2 = getforms()
-    #hparser2.feed(r2.text)
+    resisnot200 = procres(r2, debug)
+    if resisnot200:
+        return(209)
+    dprint('Looking for form with id {}'.format(FORM2), debug)
+    # re-use hparser
     hparser.feed(r2.text)
     frm2attr, frm2inps = hparser.get_forms_data() 
     dprint('Getting method, action, and input fields from the form...', debug)
-    form2_method = frm2attr['tGuardLoginForm']['method']
-    form2_action = frm2attr['tGuardLoginForm']['action']
+    form2_method = frm2attr[FORM2]['method']
+    form2_action = frm2attr[FORM2]['action']
     form2_payload = {}
-    for k in (frm2inps['tGuardLoginForm'].viewkeys()):
-        form2_payload[k] = frm2inps['tGuardLoginForm'][k]
-    if string.lower(form2_method) != 'post':
-        dprint("FORM method is not post! exiting.", debug)
-        return
-    dprint('Data payload: {}'.format(form2_payload), debug)
+    for k in (frm2inps[FORM2].viewkeys()):
+        form2_payload[k] = frm2inps[FORM2][k]
+    if string.lower(form2_method) != 'post' or form2_action in (None, '', ' '):
+        print('ERROR: FORM details are not as expected! ', frm2attr[FORM2], frm2inps[FORM2])
+        return(259)
+    #dprint('Data payload: {}'.format(form2_payload), debug)
     dprint('Connecting to {}'.format(form2_action), debug)
     # create session
     s = requests.session()
     r3 = s.post(form2_action, form2_payload)
-    printres(r3, debug)
+    resisnot200 = procres(r3, debug)
+    if resisnot200:
+        return(219)
     hparser3 = hparse.gethrefbaseondata()
     hparser3.feed(r3.text)
-    usagepage = hparser3.get_href()
-    servername3_match = re.search('(http.*://[a-zA-Z_0-9\.\-]+[:0-9]*)/.+',r3.url)
-    servername3 = servername3_match.group(1)
-    fusagepage = servername3 + usagepage
-    dprint('Connecting to {}'.format(fusagepage), debug)
-    r4 = s.get(fusagepage)
-    printres(r4, debug)
+    usagelandingpage = hparser3.get_href()
+    if usagelandingpage in (None, '', ' '):
+        print('ERROR: usagelandingpage link not found!')
+        return(299)
+    url3toks = r3.url.split('/')
+    servername3 = url3toks[0] + '//' + url3toks[2]
+    fusagelandingpage = servername3 + usagelandingpage
+    dprint('Connecting to {}'.format(fusagelandingpage), debug)
+    r4 = s.get(fusagelandingpage)
+    resisnot200 = procres(r4, debug)
+    if resisnot200:
+        return(229)
     hparser5 = hparse.getdatabaseondivid()
     hparser5.feed(r4.text)
-    usageurl = hparser5.get_udata()
-    servername4_match = re.search('(http.*://[a-zA-Z_0-9\.\-]+[:0-9]*)/.+',r4.url)
-    servername4 = servername4_match.group(1)
-    fusageurl = servername4 + usageurl
-    dprint('Connecting to {}'.format(fusageurl), debug)
-    r5 = s.get(fusageurl)
-    printres(r5, debug)
+    usagetable = hparser5.get_udata()
+    if usagetable in (None, '', ' '):
+        print('ERROR: usagetable link not found!')
+        return(349)
+    url4toks = r4.url.split('/')
+    servername4 = url4toks[0] + '//' + url4toks[2]
+    fusagetable = servername4 + usagetable
+    dprint('Connecting to {}'.format(fusagetable), debug)
+    r5 = s.get(fusagetable)
+    resisnot200 = procres(r5, debug)
+    if resisnot200:
+        return(239)
     hparser6 = hparse.gettableinfo()
     hparser6.feed(r5.text)
     totalusage, timerange, daysleft, deviceusage = hparser6.get_table_data()
-    print "Billing Period: {0}; {1}".format(timerange, daysleft)
-    print
-    print 'Detailed Data Usage Status: ', totalusage
-    print
-    print 'Detailed Data Usage per device:'
-    for k in deviceusage:
-        dphone_number = k
-        dphone_owner = deviceusage[k]['name']
-        dphone_data = deviceusage[k]['data']
-        print '\t{0} : {1} : {2}'.format(dphone_owner, dphone_number, dphone_data)
-        deviceusage[dphone_number] = [dphone_owner, dphone_data]
+    if totalusage in (None, '', ' '):
+        print('ERROR: Usage information not found!')
+        return(399)
+    dprint("Billing Period: {0}; {1}".format(timerange, daysleft), debug)
+    dprint('Detailed Data Usage Status: {}'.format(totalusage), debug)
+    dprint('Detailed Data Usage per device:', debug)
+    for dphone_number in deviceusage:
+        dphone_owner = deviceusage[dphone_number]['name']
+        dphone_data = deviceusage[dphone_number]['data']
+        dprint('\t{0} : {1} : {2}'.format(dphone_owner, dphone_number, dphone_data), debug)
+        deviceusage[dphone_number] = {'name': dphone_owner, 'data': dphone_data}
     # return 2 dictionaries.
     return { 'timerange': timerange, 'daysleft': daysleft, 'totalusage': totalusage }, deviceusage
     
